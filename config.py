f75f557a87768afe706fa14532aca2059bcd7c4b
S <s@Unknown.domain>
2018-10-26 13:15:53 -0700

    Add helpers to config.py

--- config.py
+++ config.py
@@ -1,19 +1,24 @@
-# Relative path to directory to put instances
-relPath = "../"
+# 1. EDIT THIS
+# Address of RDM controller
+backendURLBaseString = "http://IP_TO_RDM_BACKEND:9001"
 
+# Init devices list
+devices = {} # DON'T EDIT
+
+# 2. EDIT THIS
+# Add devices and options
+# EXAMPLE:
+# devices['uuid_example_1'] = {'ilocation':'192.168.0.45','account_manager':True}
+#
+devices['example_uuid_01'] = {}
+devices['example_uuid_02'] = {}
+
+# 3. OPTIONAL EDIT
 # Url of git repo to pull for instances
 repoUrl = "https://github.com/123FLO321/RealDeviceMap-UIControl.git"
 
 # Time to wait between launching instances
 startDelay = 10 # seconds
 
-# Address of RDM controller
-backendURLBaseString = "http://IP_TO_RDM_BACKEND:9001"
-
-# Init devices list
-devices = {}
-
-# Add devices and options
-#devices['uuid_example_1'] = {'ilocation':'192.168.0.45','account_manager':True}
-devices['example_uuid_01'] = {}
-devices['example_uuid_02'] = {}
+# Relative path to directory to put instances
+relPath = "../"
