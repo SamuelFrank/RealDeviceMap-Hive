d519b00d36ecddd9d404b57aed1bc87131016d4c
S <s@Unknown.domain>
2018-11-10 13:06:50 -0800

    Add the -state_one flag

--- hive.py
+++ hive.py
@@ -10,11 +10,17 @@ elif "-build" in sys.argv:
 elif "-start" in sys.argv:
 	startAll(devices)
 
+elif "-start_one" in sys.argv:
+	device_id = sys.argv.pop()
+	device = {device_id : devices[device_id]}
+	startAll(device)
+
 else:
 	print("\nHive for RealDeviceMap")
 	print("\nUsage: python hive.py -make -build -start")
 	print("\t-make\t\tMakes a new RealDeviceMap-UIControl folder to add your provisioning credentials to.")
 	print("\t-build\t\tBuilds folders for all devices and edits the files")
 	print("\t-start\t\tLaunches each device in its own window and executes python run.py")
+	print("\t-start_one\tLaunches a single device in its own window and executes python run.py (Device uuid must be last argument)")
 
 print("\nDone.")
